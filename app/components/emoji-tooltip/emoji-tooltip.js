6ecad6a0fcf02933f0b46e47ac65eefc9eee6b49
Egor Yurchenko <YegorYu@yandex.ru>
2021-06-19 22:10:43 +0300

    Выбор недавних emojis + доработка кода

--- app/components/emoji-tooltip/emoji-tooltip.js
+++ app/components/emoji-tooltip/emoji-tooltip.js
@@ -5,10 +5,18 @@ class EmojiTooltip {
     constructor(el) {
         this.emojiTooltip = el;
         this.smile = this.emojiTooltip.parentElement.querySelector('.js-message-smile');
-        this.mainBlock = this.emojiTooltip.parentElement.querySelector('.js-emoji-tooltip-main');
 
-        this.closeEmojiTooltip = true;
-        this.canToggleEmojiTooltip = true;
+        // Блоки с emojis
+        this.allEmojisBlock = this.emojiTooltip.parentElement.querySelector('.js-emoji-tooltip-all-emojis');
+        this.allEmojisBlockWrapper = this.allEmojisBlock.parentElement;
+        this.recentEmojisBlock = this.emojiTooltip.parentElement.querySelector('.js-emoji-tooltip-recent-emojis');
+        this.recentEmojisBlockWrapper = this.recentEmojisBlock.parentElement;
+
+        // Книпки переключения emojis
+        this.btnAll = this.emojiTooltip.parentElement.querySelector('.js-emoji-tooltip-btn-all');
+        this.btnRecent = this.emojiTooltip.parentElement.querySelector('.js-emoji-tooltip-btn-recent');
+
+        this.closeEmojiTooltip = true; // Закрытие tooltip?
 
         this.classes = {
             active: 'is-active',
@@ -19,27 +27,50 @@ class EmojiTooltip {
     }
 
     init() {
+        // Получаем все emojis
         axios({
-            method: this.emojiTooltip.getAttribute('data-method') || 'get',
-            url: this.emojiTooltip.getAttribute('data-url'),
+            method: this.emojiTooltip.getAttribute('data-all-emojis-method') || 'get',
+            url: this.emojiTooltip.getAttribute('data-all-emojis-url'),
         })
             .then((response) => {
                 const receivedData = response.data;
                 switch (receivedData.status) {
                     case 'GET_EMOJI_LIST_SUCCESS':
-                        this.mainBlock.innerHTML = this.getEmojiData(receivedData.data);
-                        // this.setListeners();
+                        this.allEmojisBlock.innerHTML = this.getEmojiData(receivedData.data, 'allEmojis');
+                        this.setListeners();
                         break;
                     case 'GET_EMOJI_LIST_FAIL':
                         console.error(receivedData.data.errorMessage);
-                        console.error(e);
                         break;
                     default:
-                        console.error(e);
+                        console.error('Что-то пошло не так!');
                         break;
                 }
             })
-            .catch(() => {
+            .catch((e) => {
+                console.error(e);
+            });
+            
+        // Получаем последние emoji
+        axios({
+            method: this.emojiTooltip.getAttribute('data-recent-emojis-method') || 'get',
+            url: this.emojiTooltip.getAttribute('data-recent-emojis-url'),
+        })
+            .then((response) => {
+                const receivedData = response.data;
+                switch (receivedData.status) {
+                    case 'GET_RECENT_EMOJI_LIST_SUCCESS':
+                        this.recentEmojisBlock.innerHTML = this.getEmojiData(receivedData.data, 'recentEmojis');
+                        break;
+                    case 'GET_RECENT_EMOJI_LIST_FAIL':
+                        console.error(receivedData.data.errorMessage);
+                        break;
+                    default:
+                        console.error('Что-то пошло не так!');
+                        break;
+                }
+            })
+            .catch((e) => {
                 console.error(e);
             });
     }
@@ -48,15 +79,16 @@ class EmojiTooltip {
      * Формируем объекты для каждого блока Emoji и создаём содержимое Tooltip через template
      * @param data - данные из AJAX-запроса
      */
-    getEmojiData(data) {
+    getEmojiData(data, typeEmojis) {
         const emojiListTemplate = document.getElementById('emoji-list-template');
         const tmpl = template(emojiListTemplate.innerHTML);
         let emojis = '';
 
         data.forEach((itemData, idx) => {
             const tmplData = {
+                type: typeEmojis,
                 part: idx,
-                title: itemData.title,
+                title: itemData.title || false,
                 items: itemData.items
             };
 
@@ -70,20 +102,24 @@ class EmojiTooltip {
      * Слушаем события
      */
     setListeners() {
-        this.smile.addEventListener('mouseover', () => {
-            this.openTooltop();
+        [this.smile, this.emojiTooltip].forEach(item => {
+            item.addEventListener('mouseover', () => {
+                this.openTooltop();
+            });
         });
 
-        this.smile.addEventListener('mouseout', () => {
-            this.closeTooltip();
+        [this.smile, this.emojiTooltip].forEach(item => {
+            item.addEventListener('mouseout', () => {
+                this.closeTooltip();
+            });
         });
 
-        this.emojiTooltip.addEventListener('mouseover', () => {
-            this.openTooltop();
+        this.btnAll.addEventListener('click', () => {
+            this.toggleEmojisBlock(this.btnAll, this.allEmojisBlockWrapper, this.btnRecent, this.recentEmojisBlockWrapper);
         });
 
-        this.emojiTooltip.addEventListener('mouseout', () => {
-            this.closeTooltip();
+        this.btnRecent.addEventListener('click', () => {
+            this.toggleEmojisBlock(this.btnRecent, this.recentEmojisBlockWrapper, this.btnAll, this.allEmojisBlockWrapper);
         });
     }
 
@@ -109,17 +145,29 @@ class EmojiTooltip {
 
         setTimeout(() => {
             if (this.closeEmojiTooltip) {
-                this.canToggleEmojiTooltip = false;
                 this.emojiTooltip.classList.remove(this.classes.active);
 
                 setTimeout(() => { // Задерка для плавного скрытия tooltip
                     if (this.closeEmojiTooltip) {
                         this.emojiTooltip.classList.remove(this.classes.visible);
-                        this.canToggleEmojiTooltip = true;
                     }
                 }, 200);
             }
         }, 300);
+    }
+
+    /**
+     * При нажатии на копку выбора типа emojis меняем видимость блоков
+     * @param btnActive - кнопка выбранного типа emojis, которую нужно активировать
+     * @param blockActive - блок с emojis, который нужно показать
+     * @param btnDisable - кнопка, которую нужно деактивировать
+     * @param blockDisable - блок с emojis, который нужно деактивировать
+     */
+    toggleEmojisBlock(btnActive, blockActive, btnDisable, blockDisable) {
+        btnDisable.classList.remove(this.classes.active);
+        blockDisable.classList.remove(this.classes.active);
+        btnActive.classList.add(this.classes.active);
+        blockActive.classList.add(this.classes.active);
     }
 }
 
