4c9c4394ba2fb9ca8fe94e827bacb0a9bf5a4bc0
Egor Yurchenko <YegorYu@yandex.ru>
2021-06-21 18:16:22 +0300

    Доработка css и js

--- app/components/messenger-bottom/messenger-bottom.js
+++ app/components/messenger-bottom/messenger-bottom.js
@@ -1,38 +1,42 @@
 class MessengerBottom {
     constructor(el) {
         this.messenger = el;
-        this.textarea = this.messenger.querySelector('.js-message-textarea');
-        this.smile = this.messenger.querySelector('.js-message-smile');
+        this.textarea = this.messenger.querySelector(".js-message-textarea");
+        this.smile = this.messenger.querySelector(".js-message-smile");
+
+        this.borderWidth = 2;
+        this.maxTeaxareaHeight = 200;
 
         this.setListeners();
     }
 
     /**
-     * Слушаем событие input и динамически изменяем высоту textarea (max-height = 200px)
+     * Слушаем событие input и динамически изменяем высоту textarea
+     * @returns {void}
      */
     setListeners() {
-        this.textarea.addEventListener('input', (e) => {
-            let textarea = e.target;
+        this.textarea.addEventListener("input", e => {
+            const textarea = e.target;
 
-            textarea.style.height = 'auto';
+            textarea.style.height = "auto";
 
-            let textareaHeight = textarea.scrollHeight + 2;
+            const textareaHeight = textarea.scrollHeight + this.borderWidth;
 
-            if (textareaHeight < 200) {
-                textarea.style.height = textareaHeight + "px";
-                textarea.style.overflow = 'hidden';
-                this.smile.style.right = '8px';
+            if (textareaHeight < this.maxTeaxareaHeight) {
+                textarea.style.height = `${textareaHeight}px`;
+                textarea.style.overflow = "hidden";
+                this.smile.style.right = "8px";
             } else {
-                textarea.style.height = '200px';
-                textarea.style.overflow = 'auto';
-                this.smile.style.right = '22px';
+                textarea.style.height = "200px";
+                textarea.style.overflow = "auto";
+                this.smile.style.right = "22px";
             }
-        })
+        });
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const items = document.querySelectorAll('.js-message');
+document.addEventListener("DOMContentLoaded", () => {
+    const items = document.querySelectorAll(".js-message");
     for (let i = 0; i < items.length; i++) {
         new MessengerBottom(items[i]);
     }
